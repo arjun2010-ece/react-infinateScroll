1ad9946b81d1b4158383d9c1e39abe9f9e4efa51
arjun2010-ece <arjun2010.ece@gmail.com>
2019-10-31 23:06:34 +0000

    paginated list is prepared with load more button

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+                Shiva
+            </div>
+        )
+    }
+}
+export default List;
