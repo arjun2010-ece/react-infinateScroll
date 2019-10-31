403832ccf4ff55b98bb9a7c0de374d2288e478ac
arjun2010-ece <arjun2010.ece@gmail.com>
2019-10-31 23:55:57 +0000

    react infinate scroll

--- src/App.js
+++ src/App.js
@@ -74,31 +74,31 @@ onInitialSearch = (e) => {
   }
 }
 
-const List = ({ list, page, isLoading, onPaginatedSearch }) =>
-  <div>
-    <div className="list">
-      {list.map(item => <div className="list-row" key={item.objectID}>
-        <a href={item.url}>{item.title}</a>
-      </div>)}
-    </div>
-
-    <div className="interactions">
-      {isLoading && <span>Loading...</span>}
-    </div>
-
-    <div className="interactions">
-      {
-        (page !== null && !isLoading) &&
-        <button
-          type="button"
-          onClick={onPaginatedSearch}
-        >
-          More
-        </button>
-      }
-    </div>
-
-
-  </div>
+class List extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, false);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+  onScroll = () => {
+    if (
+      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
+      this.props.list.length
+    ) {
+      this.props.onPaginatedSearch();
+    }
+  }
+  render() {
+    const { list } = this.props;
+    return (
+      <div className="list">
+        {list.map(item => <div className="list-row" key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </div>)}
+      </div>
+    );
+  };
+}
 
 export default App;
